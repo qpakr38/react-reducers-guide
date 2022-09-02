de9a3e8376c70b77238d956c69e16768babd2b11
qpakr38 <zczc0819@gmail.com>
2022-09-02 16:11:02 +0900

    FEAT : Context Componet로만들어서 데이터 연결하기

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, {useState,useEffect, useReducer} from 'react';
+import React, {useState, useEffect, useReducer, useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
     if (action.type === "USER_INPUT") {
@@ -22,6 +23,7 @@ const emailReducer = (state, action) => {
     }
 };
 const Login = (props) => {
+    const ctx = useContext(AuthContext);
     //const [enteredEmail, setEnteredEmail] = useState('');
     //const [emailIsValid, setEmailIsValid] = useState();
     //const [enteredPassword, setEnteredPassword] = useState('');
@@ -82,7 +84,7 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.val, passwordState.val);
+        ctx.onLogin(emailState.val, passwordState.val);
     };
 
     return (
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogin: () => {},
+    onLogout: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+        if (storedUserLoggedInInformation === "1") {
+            setIsLoggedIn(true);
+        }
+    }, [])
+    const loginHandler = () => {
+        setIsLoggedIn(true);
+        localStorage.setItem('isLoggedIn', '1');
+    };
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+        localStorage.removeItem('isLoggedIn');
+    };
+    return (
+        <AuthContext.Provider value={
+            {
+                isLoggedIn: isLoggedIn,
+                onLogin: loginHandler,
+                onLogout: logoutHandler
+            }
+        }>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
 
 export default AuthContext
